af1abcf78b58326906825f15137967e70975fc0a
manqobasukati <manqoba.sukati@peoplesized.com>
2020-04-24 09:53:25 +0200

    remove console statement

--- src/lib/get_keys.ts
+++ src/lib/get_keys.ts
@@ -14,7 +14,7 @@ export function get_keys(field: string, headers: string[]): string {
       return el !== null;
     });
 
-  console.log(my_headers);
+  
 
   if (my_headers[0] && my_headers[0].input) {
     return my_headers[0].input;
